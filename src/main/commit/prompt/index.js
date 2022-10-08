6614249d384f2db23408630b05bb18b0fefbedf6
Luis Float <contact@luisfloat.com>
2022-10-07 23:22:37 -0300

    feat(main): add prompt messages to commitlint

--- src/main/commit/prompt/index.js
+++ src/main/commit/prompt/index.js
@@ -1,5 +1,7 @@
 import questions from "./questions.js";
+import messages from "./messages.js";
 
 export default {
     questions,
+    messages,
 };
---
+++ src/main/commit/prompt/messages.js
@@ -0,0 +1,8 @@
+export default {
+    skip: ":skip",
+    max: "upper %d chars",
+    min: "%d chars at least",
+    emptyWarning: "can not be empty",
+    upperLimitWarning: "over limit",
+    lowerLimitWarning: "below limit",
+};
